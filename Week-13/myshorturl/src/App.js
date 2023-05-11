257de7d62a1856704aa73ab1f86ac889f6f4b6c9
HarshilVadgama31 <vadgamaharshil99@gmail.com>
2023-05-11 21:06:12 +0530

    App for Shortening URL Week-13 Assignment Done

---
+++ Week-13/myshorturl/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <LinkResult inputValue={inputValue} />
+    </div>
+  );
+}
+
+export default App;
